7a140c930a51257fca4b66c773b87ab578d9ce97
git-sudo-404 <git-sudo-404@users.noreply.github.com>
2025-11-13 19:30:21 +0530

    update : render.yml

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,7 @@
+import express, { Request, Response } from "express";
 import app from "./server";
+app.get("/api/health", (req: Request, res: Response) => {
+  res.status(200).json({ status: "ok" });
+});
 
 app.listen(8080, () => console.log("Server is listening on PORT : 8080"));
